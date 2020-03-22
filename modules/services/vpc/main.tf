fe3acba13d9e78890d0b66648813203018c0f7d4
Rick James <patrickjamesjr@protonmail.com>
2020-03-22 06:41:00 -0400

    Updated VPC Module

--- modules/services/vpc/main.tf
+++ modules/services/vpc/main.tf
@@ -1,3 +1,35 @@
-provider "aws" {
+resource "aws_vpc" "standard_vpc" {
+  cidr_block = "10.0.0.0/16"
 
+  tags = {
+    Name        = "Dev-VPC"
+    Environment = "Development"
+    Cost-Center = "PackVsPride"
+  }
+}
+
+resource "aws_subnet" "pub_sub" {
+  vpc_id = aws_vpc.standard_vpc.id
+
+  availability_zone = "us-east-1b"
+  cidr_block        = cidrsubnet(aws_vpc.standard_vpc.cidr_block, 4, 1)
+
+  tags = {
+    Name        = "pub-sub-1"
+    Environment = "Development"
+    Cost-Center = "PackVsPride"
+  }
+}
+
+resource "aws_subnet" "pri_sub" {
+  vpc_id = aws_vpc.standard_vpc.id
+
+  availability_zone = "us-east-1b"
+  cidr_block        = cidrsubnet(aws_vpc.standard_vpc.cidr_block, 4, 2)
+
+  tags = {
+    Name        = "pri-sub-1"
+    Environment = "Development"
+    Cost-Center = "PackVsPride"
+  }
 }
